21bebadc9389fbe22405b97a8615e5862c1b3edf
flynnjim <flynnlomas@hotmail.co.uk>
2024-09-07 14:12:53 +0100

    setting up hosting

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,25 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+// throw error from the notes to host
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+//
+
+// add onfig variable
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+module.exports = new Pool(config);
+
+//
+
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
