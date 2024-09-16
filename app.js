eef5a4c4aefdc43d3d3efa4e057686cd9c4254de
flynnjim <flynnlomas@hotmail.co.uk>
2024-09-16 10:24:29 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.use(express.json());
 const {getTopics, getDocumentation, getArticle, getAllArticles, getArticleComments, postComment, patchArticle, deleteComment, getUsers} = require('./controllers/index.controllers')
 
